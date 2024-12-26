49d7176363482c5254f9e2327d54d380d3e78413
aryafire1 <erthorsen876@gmail.com>
2024-12-26 17:41:01 -0500

    all inputs connected up

--- Assets/Scripts/Player Scripts/EventManager.cs
+++ Assets/Scripts/Player Scripts/EventManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
 public class EventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public InputSystemActions inputActions;
+    InputAction dash;
+    InputAction magic;
+
+
+    public static event Action DashEvent;
+    public static event Action MagicEvent;
+
+
+    void Awake() {
+        inputActions = new InputSystemActions();
+    }
+    void OnEnable() {
+        dash = inputActions.Player.Dash;
+        dash.Enable();
+        dash.performed += Dash;
+
+        magic = inputActions.Player.Magic;
+        magic.Enable();
+        magic.performed += Magic;
+    }
+    void OnDisable() {
+        dash.Disable();
+        magic.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void Dash(InputAction.CallbackContext context) {
+        Debug.Log("dash");
+        DashEvent?.Invoke();
     }
+    void Magic(InputAction.CallbackContext context) {
+        Debug.Log("magic call");
+        MagicEvent?.Invoke();
+    }
+
 }
